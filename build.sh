eb898d0284b4e333469c651a70988c113f927221
MarvinKatapult <ibingmarvin@gmail.com>
2024-03-16 18:59:53 +0100

    Changed build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 
 if [ "$1" == "-lib" ]; then
-# Create Static Lib
-g++ -c src/tmatrix.cpp -Iinclude      -o debug/tmatrix.o && \
-g++ -c src/tlist.cpp   -Iinclude      -o debug/tlist.o && \
-ar rcs terra.a debug/*
+    # Check if debug directory exists
+    if [ ! -d "./debug" ]; then
+        mkdir debug
+    fi 
+
+    # Create Static Lib
+    g++ -c src/tmatrix.cpp -Iinclude      -o debug/tmatrix.o && \
+    g++ -c src/tlist.cpp   -Iinclude      -o debug/tlist.o && \
+    ar rcs terra.a debug/*
 
 else
-# Compile all files
-g++ -o terra -Wextra -Wall -pedantic -g -Iinclude \
-   src/tmatrix.cpp \
-   src/tlist.cpp \
-   src/main.cpp
+    # Compile all files
+    g++ -o terra -Wextra -Wall -pedantic -g -Iinclude \
+       src/tmatrix.cpp \
+       src/tlist.cpp \
+       src/main.cpp
 fi
